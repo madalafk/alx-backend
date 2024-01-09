2101e5d2ec140693149a695cbad100406e7b18eb
madalafk <fmadala@hotmail.com>
2024-01-09 16:40:02 +0000

    0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Creates a single / route and an index.html template that
+simply outputs “Welcome to Holberton” as page title
+(<title>) and “Hello world” as header (<h1>).
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
